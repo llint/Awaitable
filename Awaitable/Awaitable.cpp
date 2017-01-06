e8b83490ef5d8e031e521a63cf4b5c749f259066
Lin Luo <Lin@Home>
2017-01-05 23:35:27 -0800

    Changed manual busy loop and yield to a simple timed wait

--- Awaitable/Awaitable.cpp
+++ Awaitable/Awaitable.cpp
@@ -8,19 +8,21 @@
 
 awaitable<void> set_ready_after_timeout(awaitable<int>::ref awtb, std::chrono::high_resolution_clock::duration timeout)
 {
-    auto now = std::chrono::high_resolution_clock::now();
-    while (std::chrono::high_resolution_clock::now() - now < timeout)
-        co_await awaitable<void>{}; // yield
+    co_await awaitable<void>{timeout}; // timed wait
+
     awtb.get().set_ready(123);
 }
 
 awaitable<int> named_counter(std::string name)
 {
     std::cout << "counter(" << name << ") resumed #" << 0 << std::endl;
+
     co_await awaitable<void>{2s}; // timed wait
     std::cout << "counter(" << name << ") resumed #" << 1 << std::endl;
+
     int i = co_await awaitable<int>{}; // yield, returns the default value
     std::cout << "counter(" << name << ") resumed #" << 2 << " ### " << i << std::endl;
+
     auto awtb = awaitable<int>{true}; // suspend, and returns the value from somewhere else
     set_ready_after_timeout(awtb, 3s);
     auto x = co_await awtb;
